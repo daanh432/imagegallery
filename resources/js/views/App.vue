26f960c234443739ed89064d358a036501b0b4c1
daanh432 <daan@daanhendriks.nl>
2019-07-12 12:46:34 +0200

    Icons in navbar

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -2,23 +2,23 @@
     <div>
         <div class="offcanvas-nav-primary" id="offCanvasMenu" uk-offcanvas="overlay: true; mode: reveal">
             <div class="uk-offcanvas-bar uk-flex uk-flex-column">
-                <ul class="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
+                <ul class="uk-nav uk-nav-primary uk-margin-auto-vertical uk-iconnav uk-iconnav-vertical">
                     <li>
-                        <router-link :to="{ name: 'home' }">Home</router-link>
+                        <router-link :to="{ name: 'home' }"><span uk-icon="icon: home; ratio: 2"></span> Home</router-link>
                     </li>
                     <li>
-                        <router-link :to="{ name: 'contact' }">Contact</router-link>
+                        <router-link :to="{ name: 'contact' }"><span uk-icon="icon: mail; ratio: 2"></span> Contact</router-link>
                     </li>
                     <li>
-                        <router-link :to="{ name: 'users.images.index', params: {userId: $auth.user().id}}" v-if="$auth.check()">Images</router-link>
+                        <router-link :to="{ name: 'users.images.index', params: {userId: $auth.user().id}}" v-if="$auth.check()"><span uk-icon="icon: image; ratio: 2"></span> Images</router-link>
                     </li>
                     <li class="uk-nav-header">Profile</li>
                     <li class="uk-nav-divider"></li>
                     <li v-if="$auth.check()">
-                        <router-link :to="{ name: 'users.show', params: {userId: $auth.user().id} }">{{ $auth.user().name }}</router-link>
+                        <router-link :to="{ name: 'users.show', params: {userId: $auth.user().id} }"><span uk-icon="icon: user; ratio: 2"></span> {{ $auth.user().name }}</router-link>
                     </li>
                     <li v-if="$auth.check()">
-                        <a @click.prevent="$auth.logout()" href="">Logout</a>
+                        <a @click.prevent="$auth.logout()" href=""><span uk-icon="icon: sign-out; ratio: 2"></span> Logout</a>
                     </li>
                     <li v-if="!$auth.check()">
                         <router-link :to="{ name: 'register' }">Register</router-link>
